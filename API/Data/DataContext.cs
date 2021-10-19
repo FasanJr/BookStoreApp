a6ebb9f07ee3b00158b812f0f84c6b5dfc58a42f
FasanJr <fasanpeter6@gmail.com>
2021-10-19 22:20:58 +0700

    End of Section 16

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,14 +1,18 @@
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+
 namespace API.Data
 {
   
-    public class DataContext : DbContext
+   public class DataContext : IdentityDbContext<AppUser, AppRole,int, 
+    IdentityUserClaim<int>, AppUserRole, IdentityUserLogin<int>, 
+    IdentityRoleClaim<int>, IdentityUserToken<int>>
     {
       public DataContext(DbContextOptions options) : base(options) 
       {
       }  
-      public DbSet<AppUser> Users { get; set; }
 
       public DbSet<UserLike> Likes { get; set; }
 
@@ -17,6 +21,19 @@ namespace API.Data
       protected override void OnModelCreating(ModelBuilder builder)
       {
         base.OnModelCreating(builder);
+
+        builder.Entity<AppUser>()
+        .HasMany(ur => ur.UserRoles)
+        .WithOne(u => u.User)
+        .HasForeignKey(ur => ur.UserId)
+        .IsRequired();
+
+        
+        builder.Entity<AppRole>()
+        .HasMany(ur => ur.UserRoles)
+        .WithOne(u => u.Role)
+        .HasForeignKey(ur => ur.RoleId)
+        .IsRequired();
 
         builder.Entity<UserLike>()
             .HasKey(k => new {k.SourceUserId, k.LikedUserId});
@@ -28,7 +45,7 @@ namespace API.Data
         .OnDelete(DeleteBehavior.Cascade);
 
         builder.Entity<UserLike>()
-        .HasOne(s => s.LikedUser)
+        .HasOne(s => s.LikedUser)  
         .WithMany(l => l.LikedByUsers)
         .HasForeignKey(s => s.LikedUserId)
         .OnDelete(DeleteBehavior.Cascade);
